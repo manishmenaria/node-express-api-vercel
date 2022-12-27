0bb1d012061665cbf08ab1c9db7f9448569bcc16
manishmenaria <98484872+manishmenaria@users.noreply.github.com>
2022-12-27 17:20:46 +0530

    pinata added

--- index.js
+++ index.js
@@ -3,6 +3,95 @@ const express = require('express')
 const app = express()
 const PORT = 4000
 
+
+////////////////
+
+const bodyParser = require("body-parser");
+const keys = require("./keys");
+
+// const mintNFT = require("/home/manishmenaria/MBM/Biz4Solutions Coding/Final/Part 2/Frontend/fronend/scripts/mint-nft.js");
+
+app.use(bodyParser.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true }));
+
+const pinataSDK = require("@pinata/sdk");
+
+const multer = require("multer");
+
+const axios = require("axios");
+
+const pinata = new pinataSDK(keys.ApiKey, keys.SecretKey);
+
+const fs = require("fs");
+
+const instance = axios.create({
+  maxContentLength: 100 * 1024 * 1024,
+});
+
+const upload = multer({
+  limits: {
+    fileSize: 100 * 1024 * 1024,
+  },
+  dest: "uploads/",
+});
+
+app.post("/upload", upload.single("image"), (request, response) => {
+  console.log(request.file.filename);
+
+  let imageName = request.file.filename;
+
+  const readableStreamForFile = fs.createReadStream("./uploads/" + imageName);
+
+  const options = {
+    pinataMetadata: {
+      name: "BlockchainNFT_5",
+      keyvalues: {
+        customKey: "Blue",
+        customKey2: "World",
+      },
+    },
+    pinataOptions: {
+      cidVersion: 0,
+    },
+  };
+  pinata
+    .pinFileToIPFS(readableStreamForFile, options)
+    .then((result) => {
+      //handle results here
+
+      console.log(result);
+      deleteFile("./uploads/" + imageName);
+      response.json(result);
+    })
+    .catch((err) => {
+      //handle error here
+      console.log(err);
+    });
+
+  // pinata
+  //   .testAuthentication()
+  //   .then((result) => {
+  //     //handle successful authentication here
+  //     console.log(result);
+  //   })
+  //   .catch((err) => {
+  //     //handle error here
+  //     console.log(err);
+  //   });
+});
+
+async function deleteFile(filePath) {
+  try {
+    await fs.promises.unlink(filePath);
+    console.log("File deleted successfully!");
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+
+//////////////
+
 app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT} `)
 })
@@ -14,6 +103,11 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.send('This is my about route..... ')
 })
+
+
+
+
+
 
 // Export the Express API
 module.exports = app
